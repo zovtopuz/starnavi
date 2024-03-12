b1531a058fcb021151e9f82b314d67c0b25052c6
Roman <roman.senyshyn@surelockkey.com>
2024-03-13 01:39:05 +0200

    change spred in sortByField

--- utils/sortByField.util.ts
+++ utils/sortByField.util.ts
@@ -1,6 +1,6 @@
 export const sortByField = <T,>(array: T[], field?: keyof T) => {
   if (field) {
-    return { ...array }.sort((a, b) => {
+    return [...array].sort((a, b) => {
       if (a[field] && b[field] && typeof a[field] === 'number' && typeof b[field] === 'number') {
         return Number(a[field]) - Number(b[field]);
       }
